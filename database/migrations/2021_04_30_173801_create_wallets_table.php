01919f45008e09eef7fc517e31b5e16c630423c1
Reinis Danne <rei4dan@gmail.com>
2021-04-30 21:16:53 +0300

    Models: Add Wallet class
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

---
+++ database/migrations/2021_04_30_173801_create_wallets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWalletsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(
+            'wallets',
+            function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->foreignId('user_id')->constrained('users');
+                $table->text('description');
+                $table->timestamps();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wallets');
+    }
+}
