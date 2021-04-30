7ef3160429dc90aceedcda2dca44e3cd273ba3c8
Reinis Danne <rei4dan@gmail.com>
2021-04-30 17:22:44 +0300

    webpack: Add BrowserSync
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,6 +18,7 @@ mix.js('resources/js/app.js', 'public/js')
         require('tailwindcss'),
         require('autoprefixer'),
     ])
+    .browserSync('wallet.test')
     .webpackConfig(require('./webpack.config'));
 
 if (mix.inProduction()) {
