5a3df2a8cbda4a1ae83396dbfa13730565d95d80
Reinis Danne <rei4dan@gmail.com>
2021-05-09 22:52:23 +0300

    Wallet: Reorganize layout
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- resources/js/Pages/Wallet/Show.vue
+++ resources/js/Pages/Wallet/Show.vue
@@ -1,75 +1,89 @@
 <template>
-    <div class="md:px-32 py-8 w-full">
-        <div class="text-center mb-4">
-            <span class="pr-2 font-bold">In:</span>
-            <span class="pr-4 text-green-500">
-                {{ wallet.total_in.formatted }}
-            </span>
-            <span class="pr-2 font-bold">Out:</span>
-            <span class="pr-4 text-red-500">
-                {{ wallet.total_out.formatted }}
-            </span>
-            <span class="pr-2 font-bold">Balance:</span>
-            <span
-                :class="{
-                    'text-green-500': wallet.balance.amount >= 0,
-                    'text-red-500': wallet.balance.amount < 0,
-                }"
-            >
-                {{ wallet.balance.formatted }}
-            </span>
+    <breeze-authenticated-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ wallet.name }}
+            </h2>
+        </template>
+
+        <div class="md:px-32 w-full">
+            <div class="text-center space-x-4 my-4">
+                <div class="field field-green">
+                    <span>In:</span>
+                    <span>
+                        {{ wallet.total_in.formatted }}
+                    </span>
+                </div>
+                <div class="field field-red">
+                    <span>Out:</span>
+                    <span>
+                        {{ wallet.total_out.formatted }}
+                    </span>
+                </div>
+                <div class="field">
+                    <span>Balance:</span>
+                    <span
+                        :class="{
+                            'text-green-500': wallet.balance.amount >= 0,
+                            'text-red-500': wallet.balance.amount < 0,
+                        }"
+                    >
+                        {{ wallet.balance.formatted }}
+                    </span>
+                </div>
+            </div>
+            <div class="shadow overflow-hidden rounded border-b border-gray-200">
+                <table class="table min-w-full bg-white">
+                    <thead class="bg-gray-800 text-white">
+                    <tr>
+                        <th></th>
+                        <th>Id</th>
+                        <th>Account</th>
+                        <th>Debit</th>
+                        <th>Credit</th>
+                        <th>Notes</th>
+                        <th>Date</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody class="text-gray-700">
+                    <tr v-for="transaction in wallet.transactions" :key="transaction.id" class="odd:bg-gray-100">
+                        <td class="w-8 text-center">{{ transaction.fraudulent ? '❗' : '' }}</td>
+                        <td>{{ transaction.operation_id }}</td>
+                        <td>{{ transaction.other_wallet_id ?? transaction.other }}</td>
+                        <td class="text-green-500">{{ transaction.debit?.amount }}</td>
+                        <td class="text-red-500">{{ transaction.credit?.amount }}</td>
+                        <td>{{ transaction.notes }}</td>
+                        <td class="text-center">{{ formatDateTime(transaction.created_at) }}</td>
+                        <td class="text-center">
+                            <form @submit.prevent="submit">
+                                <breeze-button class="px-2 py-1 w-24 justify-center"
+                                               @click="toggleMark(transaction.id)">
+                                    {{ transaction.fraudulent ? 'Unmark' : 'Mark' }}
+                                </breeze-button>
+                                <breeze-button class="px-2 py-1 w-24 justify-center"
+                                               @click="deleteTransaction(transaction.id)">Delete
+                                </breeze-button>
+                            </form>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <div class="shadow overflow-hidden rounded border-b border-gray-200">
-            <table class="table min-w-full bg-white">
-                <thead class="bg-gray-800 text-white">
-                <tr>
-                    <th></th>
-                    <th>Id</th>
-                    <th>Account</th>
-                    <th>Debit</th>
-                    <th>Credit</th>
-                    <th>Notes</th>
-                    <th>Date</th>
-                    <th>Action</th>
-                </tr>
-                </thead>
-                <tbody class="text-gray-700">
-                <tr v-for="transaction in wallet.transactions" :key="transaction.id" class="odd:bg-gray-100">
-                    <td class="w-8 text-center">{{ transaction.fraudulent ? '❗' : '' }}</td>
-                    <td>{{ transaction.operation_id }}</td>
-                    <td>{{ transaction.other_wallet_id ?? transaction.other }}</td>
-                    <td class="text-green-500">{{ transaction.debit?.amount }}</td>
-                    <td class="text-red-500">{{ transaction.credit?.amount }}</td>
-                    <td>{{ transaction.notes }}</td>
-                    <td class="text-center">{{ formatDateTime(transaction.created_at) }}</td>
-                    <td class="text-center">
-                        <form @submit.prevent="submit">
-                            <breeze-button class="px-2 py-1 w-24 justify-center" @click="toggleMark(transaction.id)">
-                                {{ transaction.fraudulent ? 'Unmark' : 'Mark' }}
-                            </breeze-button>
-                            <breeze-button class="px-2 py-1 w-24 justify-center"
-                                           @click="deleteTransaction(transaction.id)">Delete
-                            </breeze-button>
-                        </form>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+    </breeze-authenticated-layout>
 </template>
 
 <script>
-import Authenticated from '@/Layouts/Authenticated'
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated'
 import BreezeButton from '@/Components/Button'
 import {DateTime} from 'luxon'
 
 export default {
     name: "wallet-show",
 
-    layout: Authenticated,
-
     components: {
+        BreezeAuthenticatedLayout,
         BreezeButton,
     },
 
